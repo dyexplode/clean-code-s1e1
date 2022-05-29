674d26765fa3c4f58653e38f338fa74216dc0e6b
Vladimir <admin@ttp.by>
2022-05-29 20:11:23 +0300

    refactor: add ID in BEM style at HTML JS CSS according to rule 3.1 in html-and-css.md

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("block__new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("block__incomplete");//ul of #incomplete-tasks
+var completedTasksHolder=document.getElementById("block__completed");//completed-tasks
 
 
 //New task list item
